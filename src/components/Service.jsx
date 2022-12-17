c17fd7dfe5294f76b200dfef5f6f58007c93faab
siddu6003 <51458421+siddu6003@users.noreply.github.com>
2022-12-17 12:48:21 +0530

    services para

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -9,10 +9,7 @@ function Service(){
             <Navbar/>
             <div className="service">
                 <h2>Services</h2>
-                <p>We are proud to have a successful relationship with the customers by meeting </p>
-                <p>their expectations on quality and timely service. Our post-sales services are </p>
-                <p>offered through our well trained, highly qualified and experienced service team.</p>
-                <p> We undertake Annual Maintenance Contracts for the equipment we supply. We also provide calibration services.</p>
+                <p className="service_p">We are proud to have a successful relationship with the customers by meeting their expectations on quality and timely service. Our post-sales services are offered through our well trained, highly qualified and experienced service team. We undertake Annual Maintenance Contracts for the equipment we supply. We also provide calibration services.</p>
             </div>
             <Address/>
             <Footer/>
